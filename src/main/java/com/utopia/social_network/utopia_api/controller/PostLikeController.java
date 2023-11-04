acd4023a8071c2d814fcc76d34d0a73059bc5341
Trí Tài <77047496+tritailk65@users.noreply.github.com>
2023-11-04 12:23:41 +0700

    Update before merge

--- src/main/java/com/utopia/social_network/utopia_api/controller/PostLikeController.java
+++ src/main/java/com/utopia/social_network/utopia_api/controller/PostLikeController.java
@@ -11,6 +11,7 @@ import com.utopia.social_network.utopia_api.viewModel.SavePostLikeVM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;    
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
